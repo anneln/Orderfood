f10749b68c5440075a3e82444f6e4988d5687e89
Anne-Hélène <ahbricet@gmail.com>
2025-09-12 14:50:32 +0200

    add vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "images", // ton dossier racine "images"
+          dest: ".", // copie dans dist/images
+        },
+      ],
+    }),
+  ],
+});
